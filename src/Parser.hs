602131fcbbfcab50b20bcc96b7526089701eb526
mrsekut <k.cloudspider@gmail.com>
2020-11-17 01:08:53 +0900

    [Add]program parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,19 +1,19 @@
 module Parser where
 
 import Text.Parsec.Expr
-    ( buildExpressionParser,
-      Assoc(AssocLeft),
-      Operator(Postfix, Infix, Prefix) )
+    ( buildExpressionParser, Assoc(AssocLeft), Operator(..) )
 import Text.ParserCombinators.Parsec
-    ( alphaNum, char, lower, (<?>), (<|>), try, Parser )
+    (alphaNum, char, lower, (<?>), (<|>), try, Parser ,many, parse, ParseError)
 import qualified Text.ParserCombinators.Parsec.Token as P
 import Text.ParserCombinators.Parsec.Language
-    ( GenLanguageDef(commentStart, commentEnd, commentLine, identStart,
-                     identLetter, reservedOpNames, reservedNames, caseSensitive),
-      emptyDef )
+    ( GenLanguageDef(..), emptyDef )
 
 
--- TODO: func, call
+data Define = Fn String [Expr] Stmt
+            | Call
+            deriving Show
+
+-- TODO: loop
 data Stmt = Nop
           | String := Expr
           | If Expr Stmt Stmt
@@ -21,8 +21,8 @@ data Stmt = Nop
           | Return Expr
           | Seq [Stmt]
           deriving Show
-
 -- TODO: 型宣言 `int a,b,c,d,..;`
+-- TODO: call
 data Expr = Nat Integer
           | Var String
           | Con Bool
@@ -45,16 +45,18 @@ data BinOp = Add | Sub | Mul | Div | Rem
 def = emptyDef {
   commentStart = "/*", commentEnd = "*/", commentLine = "//",
   identStart = lower,
-  identLetter = alphaNum <|> (char '_'),
-  reservedOpNames = [
-    "++", "--",
-    "+", "-", "*", "/", "%",
-      "==", "!=", "<=", "<", ">=", ">",
-    "&&", "||", "!",
-    "=", ","],
-  reservedNames = [
-    "true", "false",
-    "if", "then", "else", "while", "loop", "return"],
+  identLetter = alphaNum <|> char '_',
+  reservedOpNames =
+    [ "++", "--"
+    , "+", "-", "*", "/", "%"
+    , "==", "!=", "<=", "<", ">=", ">"
+    , "&&", "||", "!"
+    , "=", ","
+    ],
+  reservedNames =
+    [ "true", "false"
+    , "if", "then", "else", "while", "loop", "return", "fn"
+    ],
   caseSensitive = True
 }
 
@@ -65,6 +67,83 @@ reservedOp = P.reservedOp lexer
 reserved = P.reserved lexer
 natural = P.natural lexer
 semi = P.semi lexer
+braces = P.braces lexer
+commaSep = P.commaSep lexer
+whiteSpace = P.whiteSpace lexer
+
+
+
+-- Program
+
+parseProgram :: String -> Either ParseError [Define]
+parseProgram = parse (whiteSpace >> many parseFn) "myparser"
+
+
+
+-- Function
+
+
+parseFn :: Parser Define
+parseFn = do
+  reserved "fn"
+  name <- identifier
+  args <- parens $ commaSep parseExpr
+  Fn name args <$> parseSeq
+
+
+-- Statement
+
+
+parseSeq :: Parser Stmt
+parseSeq = braces $ do
+  list <- many parseStmt
+  return $ if null list then Nop else Seq list
+
+
+parseStmt :: Parser Stmt
+parseStmt = parseIf
+        <|> parseSeq
+        <|> parseAssign
+        <|> parseWhile
+        <|> parseReturn
+        <|> parseNop
+
+
+parseIf :: Parser Stmt
+parseIf = do
+  reserved "if"
+  cond <- parens parseExpr
+  s1 <- parseStmt
+  s2 <- try (reserved "else" >> parseStmt) <|> pure Nop
+  pure $ If cond s1 s2
+
+
+parseWhile :: Parser Stmt
+parseWhile = do
+  reserved "while"
+  cond <- parens parseExpr
+  While cond <$> parseStmt
+
+
+parseReturn :: Parser Stmt
+parseReturn = do
+  reserved "return"
+  expr <- parseExpr
+  semi
+  return $ Return expr
+
+
+parseAssign :: Parser Stmt
+parseAssign = do
+  name <- identifier
+  reservedOp "="
+  expr <- parseExpr
+  semi
+  return $ name := expr
+
+
+parseNop :: Parser Stmt
+parseNop = semi >> pure Nop
 
 
 
@@ -93,6 +172,7 @@ postfix name fun = Postfix (reservedOp name >> pure fun)
 
 -- Term
 
+-- TODO: call
 parseTerm =
   parens parseExpr
     <|> Nat <$> natural
