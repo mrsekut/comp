45c8501fe143f6c54fc6dabd03ade88d7e61204e
mrsekut <k.cloudspider@gmail.com>
2020-12-11 00:51:09 +0900

    [add]CodeGen

---
+++ src/CodeGen.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE OverloadedStrings #-}
+module CodeGen where
+
+import Control.Monad.RWS
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Text (Text)
+import AST.Define ( Define(..), Stmt(..), Expr(..)
+                  , UniOpS(..), UniOpE(..), BinOp(..)
+                  )
+
+
+
+-- W
+
+type W a = RWST () Text State IO a
+data State = State { variableCount :: Int }
+
+
+
+-- Reifiable
+
+class Reifiable a where
+    reify :: a -> W Text
+
+instance Reifiable Define where
+    reify (Fn f as b) = do
+        args <- mapM reify as
+        let params = T.intercalate ", " args
+        body <- reify b
+        brs <- braces body
+        pure $ T.concat ["fn ", T.pack f, "(", params, ")", brs]
+
+instance Reifiable Stmt where
+    reify (Assign v e) = do
+        exp <- reify e
+        pure $ T.concat [T.pack v,"=",exp]
+    reify (If c t e) = do
+        cnd <- reify c
+        thn <- reify t
+        els <- reify e
+        pure $ T.concat ["if (", cnd, ") {", thn , "} else {", els, "}"]
+    reify (While c b) = do
+        cnd <- reify c
+        body <- reify b
+        brs <- braces body
+        pure $ T.concat ["while (", cnd, ")", brs]
+    reify (Return r) = do
+        rtn <- reify r
+        pure $ T.concat ["return (", rtn, ")"]
+    reify (Seq ss) = do
+        stmts <- mapM reify ss
+        pure $ T.concat[T.intercalate ";\n" stmts, ";\n"]
+    reify (Loop c b) = do
+        cnt <- reify c
+        body <- reify b
+        brs <- braces body
+        pure $ T.concat ["loop (", cnt, ")", brs]
+    reify (Init vs) = do
+        stmts <- mapM reify vs
+        pure $ T.concat[T.intercalate "," stmts]
+    reify (UnoS op e) = do
+        exp <- reify e
+        pure $ T.concat [exp, unoS op]
+    reify Nop = pure $ T.pack ""
+
+
+instance Reifiable Expr where
+    reify (Nat i) = pure $ T.pack $ show i
+    reify (Var v) = pure $ T.pack v
+    reify (Con b) = pure $ T.pack $ show b
+    reify (UnoE op e) = do
+        exp <- reify e
+        pure $ T.concat [unoE op, exp]
+    reify (Bio op e1 e2) = do
+        exp1 <- reify e1
+        exp2 <- reify e2
+        pure $ T.concat [exp1, bio op, exp2]
+    reify (Call f ps) = do
+        params <- mapM reify ps
+        pure $ T.concat [T.pack f, "(", T.intercalate ";\n" params, ")", ";"]
+
+
+
+-- Utils
+
+unoS :: UniOpS -> Text
+unoS Inc = "++"
+unoS Dec = "--"
+
+
+unoE :: UniOpE -> Text
+unoE Not = "!"
+unoE Neg = "-"
+
+bio :: BinOp -> Text
+bio Add = "+"
+bio Sub = "-"
+bio Mul = "*"
+bio Div = "/"
+bio Rem = "%"
+bio Lt  = "<"
+bio Le  = "<="
+bio Gt  = ">"
+bio Ge  = ">="
+bio Eq  = "="
+bio Neq = "!="
+bio And = "&&"
+bio Or  = "||"
+
+braces :: Text -> W Text
+braces body = do
+    pure $ T.concat [" {\n", body, "}\n"]
+
+gen :: Define -> IO ()
+gen e = do
+    (txt,_,assigned) <- runRWST (reify e) () (State 0)
+    T.putStrLn $ T.append assigned txt
