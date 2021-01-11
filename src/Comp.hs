0a92071961e67954285d6cc4ebd5de825a52632a
mrsekut <k.cloudspider@gmail.com>
2021-01-12 00:46:16 +0900

    refactor: ToProgram and Comp

--- src/Comp.hs
+++ src/Comp.hs
@@ -2,6 +2,8 @@ module Comp (execute) where
 
 import AST.Program (Program(..), Vars, Cmd(..))
 import qualified Data.Map as M
+import qualified Code as C
+import qualified ToProgram as T
 import Data.Maybe (fromJust)
 
 
@@ -15,7 +17,6 @@ comp = undefined
 -- initialize :: Program -> Vars
 -- initialize (ProgramCode _ m _) = M.fromList (M (,0) [1..m])
 
-
 execute :: Program -> Integer
 execute (Program vs cmds) = execute' cmds 1 vs
 
@@ -24,11 +25,10 @@ execute (Program vs cmds) = execute' cmds 1 vs
 execute' :: [Cmd] -> Integer -> Vars -> Integer
 execute' [] _ _ = undefined
 execute' cmd pc vs =
-    if pc == toInteger(length cmd) -- NOTE: Startがいるので。いないなら+1
-    then fromJust $ M.lookup 1 vs
-    else execute' cmd next nvs
-        where (next, nvs) = row (cmd !! fromInteger pc, vs) pc
-
+  if pc == toInteger(length cmd) -- NOTE: Startがいるので。いないなら+1
+  then fromJust $ M.lookup 1 vs
+  else execute' cmd next nvs
+    where (next, nvs) = row (cmd !! fromInteger pc, vs) pc
 
 
 -- FIXME: clean
@@ -39,14 +39,17 @@ row (BindV a b, vs) pc = (pc+1, bind a b vs)
 row (Inc a, vs)     pc = (pc+1, M.update inc a vs)
 row (Dec a, vs)     pc = (pc+1, M.update dec a vs)
 row (If n l, vs)    pc = case M.lookup n vs of
-    Just x -> if x>0 then (l, vs) else (pc+1, vs)
-    Nothing -> error "wwwww"
+  Just x -> if x>0 then (l, vs) else (pc+1, vs)
+  Nothing -> error "wwwww"
+
 
 inc :: Num a => a -> Maybe a
 inc n = Just (n+1)
 
+
 dec :: (Ord a, Num a) => a -> Maybe a
 dec n = if n > 0 then Just (n-1) else Just n
+
 
 bind :: Ord a => a -> a -> M.Map a a -> M.Map a a
 bind a b vs = M.insert (fromJust $ M.lookup b vs) a vs
--- src/ToProgram.hs
+++ src/ToProgram.hs
@@ -5,8 +5,7 @@ import qualified Data.Map as M
 import qualified Code as C
 
 
-type Validation = Either -- FIXME: 後で消す
-type Result a = Validation [String] a
+type Result a = Either [String] a
 
 
 
@@ -36,16 +35,18 @@ data ProgramCode = ProgramCode InputCode VarsCode CmdsCode deriving (Show)
 -- lengthが3であるのチェックも兼ねる
 toProgramCode :: C.Code -> Result ProgramCode
 toProgramCode c = case length d of
-    3 -> Right $ ProgramCode (d!!0) (d!!1) (d!!2)
-    _ -> Left ["not Program"]
-    where d = C.decode c
+  3 -> Right $ ProgramCode (d!!0) (d!!1) (d!!2)
+  _ -> Left ["not Program"]
+  where d = C.decode c
 
 
 toProgram :: ProgramCode -> Program
 toProgram (ProgramCode k m s) = Program (initializeVars k m) (makeCmds s)
 
+
 initializeVars :: InputCode -> VarsCode -> Vars
 initializeVars k m = M.fromList ([(v,0) | v <- [0..k+m]])
+
 
 makeCmds :: CmdsCode -> [Cmd]
 makeCmds c = map toCmd $ C.decode c
@@ -53,42 +54,48 @@ makeCmds c = map toCmd $ C.decode c
 
 toCmd :: C.Code -> Cmd
 toCmd c = case head d of
-    1 -> checkType1 d
-    2 -> checkType2 d
-    3 -> checkType3 d
-    4 -> checkType4 d
-    5 -> checkType5 d
-    6 -> checkType6 d
-    _ -> error "invariant program type"
-    where d = C.decode c
+  1 -> checkType1 d
+  2 -> checkType2 d
+  3 -> checkType3 d
+  4 -> checkType4 d
+  5 -> checkType5 d
+  6 -> checkType6 d
+  _ -> error "invariant program type"
+  where d = C.decode c
+
 
 checkType1 :: [Integer] -> Cmd
 checkType1 ix = if length ix == 2
-    then Goto (ix!!1)
-    else error "type error 1"
+  then Goto (ix!!1)
+  else error "type error 1"
+
 
 checkType2 :: [Integer] -> Cmd
 checkType2 ix = if length ix == 3
-    then Bind (ix!!1) (ix!!2)
-    else error "type error 2"
+  then Bind (ix!!1) (ix!!2)
+  else error "type error 2"
+
 
 checkType3 :: [Integer] -> Cmd
 checkType3 ix = if length ix == 3
-    then BindV (ix!!1) (ix!!2)
-    else error "type error 3"
+  then BindV (ix!!1) (ix!!2)
+  else error "type error 3"
+
 
 checkType4 :: [Integer] -> Cmd
 checkType4 ix = if length ix == 2
-    then Inc (ix!!1)
-    else error "type error 4"
+  then Inc (ix!!1)
+  else error "type error 4"
+
 
 checkType5 :: [Integer] -> Cmd
 checkType5 ix = if length ix == 2
-    then Dec (ix!!1)
-    else error "type error 5"
+  then Dec (ix!!1)
+  else error "type error 5"
+
 
 checkType6 :: [Integer] -> Cmd
 checkType6 ix = if length ix == 3
-    then If (ix!!1) (ix!!2)
-    else error "type error 6"
+  then If (ix!!1) (ix!!2)
+  else error "type error 6"
 
